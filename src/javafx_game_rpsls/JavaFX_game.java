2f2b565107f577cbd304b1ab86b21967978385b1
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-10-14 00:41:31 -0400

    Created the UI elements including buttons, images, and imageViews

--- src/javafx_game_rpsls/JavaFX_game.java
+++ src/javafx_game_rpsls/JavaFX_game.java
@@ -6,6 +6,14 @@ https://www.tutorialspoint.com/how-to-add-an-image-to-a-button-action-in-javafx
  */
 package javafx_game_rpsls;
 
+import java.io.FileInputStream;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 /**
  * Name: Muhaddatha Abdulghani
  * Professor: Dr. John Baugh
@@ -13,8 +21,47 @@ package javafx_game_rpsls;
  * Description:
  * 
  */
-public class JavaFX_game {
-    public static void main(String[] args){
-        
+public class JavaFX_game extends Application {
+    
+    int imageHeight = 20;
+    int buttonSize = 50;
+    
+    private Label ComputerPickLabel = new Label("Computer");
+    private Label PlayerPickLabel = new Label("Player");
+    private Label gameResult = new Label();
+    private Label selectOptionLabel = new Label("Please selct your weapon of choice");
+    
+    //Creating images
+    private Image rockImage = new Image("images/rock.jpg"); //need to specify path still
+    ImageView rockView = new ImageView(rockImage);
+    private Image paperImage = new Image("images/paper.jpg");
+    ImageView paperView = new ImageView(paperImage);
+    private Image scissorsImage = new Image("images/paper.jpg");
+    ImageView scissorsView = new ImageView(scissorsImage);
+    private Image lizardImage = new Image("images/scissors.jpg");
+    ImageView lizardView = new ImageView(lizardImage);
+    private Image spockImage = new Image("images/spock.jpg");
+    ImageView spockView = new ImageView(spockImage);
+    
+    Button rockButton = new Button();
+    Button paperButton = new Button();
+    Button scissorsButton = new Button();
+    Button lizardButton = new Button();
+    Button spockButton = new Button();
+    
+    
+    
+
+    @Override//start method from the Application class needs to be overridden
+    public void start(Stage stage) throws Exception {
+       
     }
+    
+    
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+    
 }
